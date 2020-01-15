094453297d04cb4a62c202ae368c44ac4d9a3944
Romuald <robindranat@gmail.com>
2020-01-15 23:12:04 +0300

    add modal cart

--- loft-taxi/src/components/profile.js
+++ loft-taxi/src/components/profile.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from "./Header";
+import Btn from "../shared/Btn";
 
 
 const Profile = () => <div className={'profile__bg'}>
     <Header/>
     <div className={'profile'}>
         <div className={'profile__content'}>
-            <h1>Тут модалка профиля</h1>
+            <div className={'profile__modal'}>
+                <div className={'profile__modal-title'}>Профиль</div>
+                <div className={'profile__modal-text'}>Способ оплаты</div>
+                <div className={'profile__cartblock'}>
+                    <div className={'profile__cart'}>
+                        <div className={'profile__cart-item'}></div>
+                    </div>
+                    <div className={'profile__cart'}>
+                        <div className={'profile__cart-item'}></div>
+                    </div>
+                </div>
+                <div className={'profile__modal-btns'}>
+                    <Btn link={'/profile'} bnttext={'Сохранить'}/>
+                </div>
+            </div>
         </div>
     </div>
 
