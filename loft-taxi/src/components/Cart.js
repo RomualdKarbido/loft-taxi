7f7a21dfad1b7db2aaa68d73969cf4abe31b3f6a
Romuald <robindranat@gmail.com>
2020-01-16 20:07:23 +0300

    123

--- loft-taxi/src/components/Cart.js
+++ loft-taxi/src/components/Cart.js
@@ -10,14 +10,16 @@ function Cart(props) {
                 <div className={'cart__line'}>
                     <TextField
                         id="numcart"
-                        value="1234 1234 1222 1223"
+                        defaultValue="1234 1234 1222 1223"
+                        placeholder="1234 1234 1222 1223"
                         label="Номер карты"/>
                 </div>
                 <div className={'cart__line'}>
                     <div className={'cart__line-short'}>
                         <TextField
                             id="start"
-                            value={'00/00'}
+                            defaultValue={'00/00'}
+                            placeholder={'00/00'}
                             label="Срок действия"/>
                     </div>
                 </div>
@@ -31,14 +33,17 @@ function Cart(props) {
                 <div className={'cart__line'}>
                     <TextField
                         id="nameuser"
-                        value="USER NAME"
+                        defaultValue="USER NAME"
+                        placeholder="USER NAME"
+                        type={'text'}
                         label="Имя владельца"/>
                 </div>
                 <div className={'cart__line'}>
                     <div className={'cart__line-short'}>
                         <TextField
                             id="cvc"
-                            value={'123'}
+                            defaultValue={'123'}
+                            placeholder={'123'}
                             type={'password'}
                             label=" CVC"/>
                     </div>
